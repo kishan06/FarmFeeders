155f8a394645391a17d3a561afc77b1abbe96b77
Shubhamshirva <shubhamshirva@gmail.com>
2023-04-26 19:29:07 +0530

    splash slider, notification page and settings page completed

--- lib/resources/routes/route_name.dart
+++ lib/resources/routes/route_name.dart
@@ -2,4 +2,7 @@ class RouteName {
   static const String splashScreen = '/';
   static const String loginScreen = '/loginScreen';
   static const String splashslider = '/splashslider';
+  static const String notification = '/notification';
+  static const String settings = '/settings';
+
 }
