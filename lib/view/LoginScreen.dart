417d23974096f75ac4408da83c395d24b9c9aba5
rajshinde046 <rajshinde046@gmail.com>
2023-04-26 14:58:57 +0530

    textformfield

--- lib/view/LoginScreen.dart
+++ lib/view/LoginScreen.dart
@@ -1,4 +1,6 @@
+import 'package:farmfeeders/common/CommonTextFormField.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
@@ -28,63 +30,110 @@ class _LoginScreenState extends State<LoginScreen> {
             headerSliverBuilder: (BuildContext context, bool boxIsScrolled) {
               return <Widget>[
                 SliverList(
-                    delegate: SliverChildBuilderDelegate(
-                        childCount: 1,
-                        (context, index) => Container(
-                              height: 300,
-                              color: Color(0xFF0E5F02),
-                              child: Column(
-                                //mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Stack(
-                                    //  alignment: Alignment.topCenter,
-                                    children: [
-                                      Positioned(
-                                        left: -15 * fem,
-                                        top: -15 * fem,
-                                        child: SvgPicture.asset(
-                                          "assets/grass.svg",
-                                        ),
-                                      ),
-                                      Positioned(
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.start,
-                                          children: [
-                                            SizedBox(
-                                              height: 50,
-                                            ),
-                                            Center(
-                                              child: Image.asset(
-                                                "assets/logo.png",
-                                                height: 200,
-                                              ),
-                                            ),
-                                            SizedBox(
-                                              height: 50,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
+                  delegate: SliverChildBuilderDelegate(
+                    childCount: 1,
+                    (context, index) => Container(
+                      height: 250,
+                      color: Color(0xFF0E5F02),
+                      child: Column(
+                        //mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Stack(
+                            //  alignment: Alignment.topCenter,
+                            children: [
+                              Positioned(
+                                left: -15 * fem,
+                                top: -15 * fem,
+                                child: SvgPicture.asset(
+                                  "assets/grass.svg",
+                                ),
                               ),
-                            ))),
+                              Positioned(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      height: 25,
+                                    ),
+                                    Center(
+                                      child: Image.asset(
+                                        "assets/logo.png",
+                                        height: 200,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 25,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ];
             },
             body: Container(
               height: double.infinity,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20))),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   SizedBox(
                     height: 20,
                   ),
+                  Text("Let's sign you in"),
+                  // CustomTextFormField(
+                  //   hintText: '',
+                  //   validatorText: '',
+                  // ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(
+                              color: Color(0xFF707070), width: 1),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(
+                              color: Color(0xFF707070), width: 1),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(
+                              color: Color(0xFF707070), width: 1),
+                        ),
+                        errorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide:
+                              const BorderSide(color: Colors.red, width: 1),
+                        ),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide:
+                              const BorderSide(color: Colors.red, width: 1),
+                        ),
+                      ),
+                    ),
+                  ),
+                  CustomTextFormField(
+                    leadingIcon: Icon(Icons.abc),
+                    hintText: "hintText",
+                    validatorText: "validatorText",
+                    isInputPassword: true,
+                  )
                 ],
               ),
             ),
