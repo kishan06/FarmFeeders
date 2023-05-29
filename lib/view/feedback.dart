36de61e17213247041344d06f6ff831098bc55dc
shams7khan <khanshamshad648@gmail.com>
2023-05-29 13:49:26 +0530

    for splash

--- lib/view/feedback.dart
+++ lib/view/feedback.dart
@@ -7,8 +7,10 @@ import 'package:farmfeeders/common/CommonTextFormField.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
+import 'package:scroll_snap_list/scroll_snap_list.dart';
 
 class FeedBack extends StatefulWidget {
   const FeedBack({super.key});
@@ -19,13 +21,30 @@ class FeedBack extends StatefulWidget {
 
 class _FeedBackState extends State<FeedBack> {
   TextEditingController phoneController = TextEditingController();
+  int focusedValue = 0;
+  List feedbackData = [
+    {"image":"assets/images/excellent.svg","text":"It Was Excellent"},
+    {"image":"assets/images/good.svg","text":"It Was Good"},
+    {"image":"assets/images/medium.svg","text":"It Was Medium"},
+    {"image":"assets/images/poor.svg","text":"It Was Poor"},
+    {"image":"assets/images/bad.svg","text":"It Was Very Bad"},
+    // {"image":"","text":""},
+
+
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppColors.white,
-        title: customAppBar(text: "Feedback"),
+        title: customAppBar(text: "Feedback",
+          actions: true,
+          icon: Icon(Icons.more_vert,
+            size: 35.h,
+            color: AppColors.black,
+          )
+        ),
 
         // backgroundColor: Color(0xFFF5F8FA),
         elevation: 0,
@@ -41,67 +60,119 @@ class _FeedBackState extends State<FeedBack> {
             // mainAxisAlignment: MainAxisAlignment.end,
             children: [
               Column(
-                // crossAxisAlignment: CrossAxisAlignment.start,
+                // crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  // Lottie.asset(
+                  // Lottie.asset(it was
                   //   "assets/lotties/FeedBack.json",
                   //   width: 200.w,
                   //   height: 200.w,
                   // ),
                   sizedBoxHeight(25.h),
-
+                          
                   textBlack20W7000Mon("Rate your experience"),
+                          
+                  textBlack16W5000(
+                    "How did you feel while using farmflow?",
+                  ),
+
+                  Row(),
+
+                  sizedBoxHeight(80.h),
+
+                  SvgPicture.asset(feedbackData[focusedValue]["image"],
+                    height: 82.h,
+                    width: 82.h,
+                  ),
+
+                  sizedBoxHeight(15.h),
+
+                  textBlack18W7000(feedbackData[focusedValue]["text"]),
+
+                  sizedBoxHeight(20.h),
+
+                  Icon(Icons.expand_more,
+                    size: 50.h,
+                  ),
+
+                  // SizedBox(
+                  //   height: 500.h,
+                  //   // height =  200.h,
+                  //   child: ListWheelScrollView(
+                  //     // scrollBehavior: ,
+
+                  //     itemExtent: 82.h, 
+                  //     children: List.generate(10, (index) => textBlack10("2"))
+                  //   ),
+                  // )
 
                   SizedBox(
-                    // width: 270.w,
-                    child: textBlack16W5000(
-                      "lorem ipsum is dummy of the printing?",
+                    height: 250.h,
+                    child: Container(
+                      // color: AppColors.redFA5658,
+                      child: ScrollSnapList(
+                        itemBuilder: _buildItemList,
+                        itemSize: 150,
+                      curve: Curves.decelerate,
+                      dynamicItemSize: true,
+                      onReachEnd: (){
+                          print('Done!');
+                      },
+                      itemCount: 5,
+                      onItemFocus: (val){
+                        setState(() {
+                          focusedValue = val;
+                        });
+                        // focusedValue = val;
+                      },
+                    )
                     ),
                   ),
 
-                  sizedBoxHeight(35.h),
+                  SizedBox(
+                    width: 300.w,
+                    child: customButtonCurve(text: "Next")
+                  )
 
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: textBlack16W5000("Phone Number"),
-                  ),
 
-                  sizedBoxHeight(8.h),
-
-                  CustomTextFormField(
-                      texttype: TextInputType.phone,
-                      textEditingController: phoneController,
-                      inputFormatters: <TextInputFormatter>[
-                        FilteringTextInputFormatter.digitsOnly,
-                        LengthLimitingTextInputFormatter(10),
-                      ],
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return "Please Enter a Phone Number";
-                        } else if (!RegExp(
-                                r'^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$')
-                            .hasMatch(value)) {
-                          return "Please Enter a Valid Phone Number";
-                        }
-                        return null;
-                      },
-                      hintText: "Enter your Phone Number",
-                      validatorText: "Enter your Phone Number"),
-                  // Spacer(),
-
-                  sizedBoxHeight(130.h),
-
-                  customButtonCurve(
-                      text: "Next",
-                      onTap: () {
-                        Get.toNamed("/verifyNumber");
-                      }),
+                          
+                  
                 ],
               ),
             ],
           ),
         ),
       )),
+    );
+  }
+
+  List<int> data = [
+    10,9,8,7,6,5,4,3,2,1
+  ];
+
+  Widget _buildItemList(BuildContext context, int index){
+    if(index == data.length)
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    return Container(
+      width: 150,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(feedbackData[index]["image"],
+            height: 82.h,
+            width: 82.h,
+          ),
+          // Container(
+          //   color: Colors.yellow,
+          //   width: 150,
+          //   height: 200,
+          //   child: Center(
+          //     child: Text('${data[index]}',style: TextStyle(fontSize: 50.0,color: Colors.black),),
+          //   ),
+          // ),
+        ],
+      ),
     );
   }
 }
