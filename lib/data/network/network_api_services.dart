6ecb163427e31eff907ab51f5943e2aaa8b9a2b0
rajshinde046 <rajshinde046@gmail.com>
2023-10-18 18:37:28 +0530

    contact us

--- lib/data/network/network_api_services.dart
+++ lib/data/network/network_api_services.dart
@@ -51,9 +51,8 @@ class NetworkApiServices extends BaseApiServices {
       response = await dio.get(url,
           options: Options(headers: {
             'method': 'POST',
-            "authorization":
-                "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiNWYyOTU2MzU3N2ViYjI2YTY2YmY1ODA5ZmE2ODk1MTkwNDY4ZmE3ZWNkMGJjN2YxNzJmMjgwODlkMDZjYTMxNDRhOWNhODg2NWMwYjllMTAiLCJpYXQiOjE2OTcwOTcyNzQuMzAzNDg5OTIzNDc3MTcyODUxNTYyNSwibmJmIjoxNjk3MDk3Mjc0LjMwMzQ5MTExNTU3MDA2ODM1OTM3NSwiZXhwIjoxNzI4NzE5Njc0LjMwMDU1NDk5MDc2ODQzMjYxNzE4NzUsInN1YiI6Ijg5Iiwic2NvcGVzIjpbIioiXX0.S3nliV2jHB97526ubyn0zSp01gPa4462wxJAiUsKrQ-ofPM-7dOstij9NrsApxvY7Qp6In_cayOSAJWNrcz8FKCsB9ZwJagNDdd2BVn6TxRTTIkLOQABzveHR2z-uAx2ZPxAfRCn6pQLWniAsAnxa7wpsujSXocvvg4E0OLdnmKmN-4N24b3CccNEuaPVBUAy0FkG4BU5mZyV_JZ099w5Ck2X68xcd-QoHXOuYp-vF9DZw6HcdW2xx84fXY0Lss3BDQBEk8XdqMFrG-s8zyiHYCSrarPxS8PKYJ6Q87AgyX-f2glRYLgc5xOqAMZI8L8EBzAiOlvp33pc2m_4y7y-jZtHTARBJ09LhZUiresn45uYndEfV-Sx5uHp7QIyXFVkZqc4rXUdwXCsVFq44FRQgQ3A6Q-WzUEphC2d5tnUZxOJL-RmNoM7IO9bzeeCbwGlLU200KzMLRqWv0-G7CX7mwPFsaI1iau65cg_eIrbX_U2oRTDaIKX2gIH7h4t7-d3WhmkySxWeVdUWu66LgDK9hyPEensGrZLZW5KJUBYYSRjyD9OCdXIZicJN0Jv1z0aXaMIqz9hbVZYg8Np-5GaulzfQa5YCxQYzW6JwVXVL1SV_WQyqi35240UPIlvjPb0lFarNdfIloJRfs_y5fwK3rS9zCeWuriJ-SfBehZqvA",
-            // 'access-token':
+            "authorization": basicAuth,
+            'access-token': token
 
             // "device-id": deviceId
           }));
@@ -89,16 +88,20 @@ class NetworkApiServices extends BaseApiServices {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('token');
     try {
-      response = await dio.post(url,
-          data: data,
-          options: token != null
-              ? Options(headers: {
-                  "authorization": "Bearer $token",
-                  //'access-token': token,
-                })
-              : Options(headers: {
+      response = await dio.post(
+        url,
+        data: data,
+        options: token != null
+            ? Options(headers: {
+                "authorization": "Bearer $token",
+                //'access-token': token,
+              })
+            : Options(
+                headers: {
                   "authorization": basicAuth,
-                }));
+                },
+              ),
+      );
     } on Exception catch (_) {
       return ResponseData<dynamic>(
           'Oops something Went Wrong', ResponseStatus.FAILED);
---
+++ lib/view_models/ContactusAPI.dart
@@ -0,0 +1,27 @@
+import 'package:farmfeeders/Utils/base_manager.dart';
+import 'package:farmfeeders/data/network/network_api_services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ContactusAPI {
+  ContactusAPI(this.data);
+  var data;
+  Future<ResponseData<dynamic>> contactusApi() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final response = await NetworkApiServices().postApi(
+      data,
+      "https://farmflow.betadelivery.com/api/contact",
+    );
+
+    if (response.status == ResponseStatus.SUCCESS) {
+      Map<String, dynamic> responseData =
+          Map<String, dynamic>.from(response.data);
+      if (responseData['success']) {
+        print("success $response");
+      } else {
+        return ResponseData<dynamic>(
+            responseData['message'], ResponseStatus.FAILED);
+      }
+    }
+    return response;
+  }
+}
