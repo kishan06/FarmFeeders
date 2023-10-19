d2c5597fd9c5adf2216d8edf92fe857d5d6c9f22
shams7khan <khanshamshad648@gmail.com>
2023-10-19 19:52:27 +0530

    feed info

--- lib/Utils/api_urls.dart
+++ lib/Utils/api_urls.dart
@@ -2,5 +2,7 @@ class ApiUrls {
   static const _base = "https://farmflow.betadelivery.com/api/";
   static const liveStockGet = _base + "livestock/data/";
   static const liveStockSet = _base + "store/livestock-info";
+  static const getFeedInfoDropdownData = _base + "feed/data";
+  static const setFeedInfo = _base + "store/feed-info";
   
 }
--- lib/common/flush_bar.dart
+++ lib/common/flush_bar.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:another_flushbar/flushbar.dart';
 
-Widget commonFlushBar(BuildContext context, {required String msg}){
+Widget commonFlushBar(BuildContext context, {String? title, required String msg}){
   return Flushbar(
-    title:  "Error",
+    title: title == null ? "Error" : title,
     message:  msg,
     duration:  Duration(seconds: 3),
   )..show(context);
