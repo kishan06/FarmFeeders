b26fa43c45f1c4d2a6b7c11d06a29ad12b782acf
rajshinde046 <rajshinde046@gmail.com>
2023-10-20 15:31:58 +0530

    login

--- lib/data/network/network_api_services.dart
+++ lib/data/network/network_api_services.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:farmfeeders/Utils/base_manager.dart';
+import 'package:farmfeeders/Utils/global.dart';
 import 'package:farmfeeders/data/network/base_api_services.dart';
 import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -88,8 +89,7 @@ class NetworkApiServices extends BaseApiServices {
     try {
       response = await dio.get(url,
           options: Options(headers: {
-            'authorization':
-                "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiNWE1ZTNlYmI0YzM1YjUzNzdlMjA0MDgwNzI0MGQyZTM0Y2QyZTA4ZDY5ZDQ0NzFiZDQ4MGJlODdjODFhNzE3YWE1MzE4ZmNkMzdmNmFlYzkiLCJpYXQiOjE2OTY1MDM0ODIuNjA5NDI1MDY3OTAxNjExMzI4MTI1LCJuYmYiOjE2OTY1MDM0ODIuNjA5NDI2OTc1MjUwMjQ0MTQwNjI1LCJleHAiOjE3MjgxMjU4ODIuNjA2NTg5MDc4OTAzMTk4MjQyMTg3NSwic3ViIjoiODAiLCJzY29wZXMiOlsiKiJdfQ.NiUl7xO3z2-9jc7LaQbakYGYwDNXs6vzpF6gDN5anX21d7GlMTu-CIDGX5CqAiQ-5AT-B1egdfnFbXGXmeddnUqUAZGnWB9sCVnxnNA0grMNLVG5dKCE20Tc-_dMDanyV13cNelbwiI13W_Vur-8RXnCwi1cF1jl_-QSWoTl6R3QOm49Iv10kp_8KmMILRAgeEkRFUiCZpB4FdurVB5qALSY-mDeiBPPeZLQkvYF3p1xu4K0mQdtX1kxo4HG-JxoB3jSwasizlqkJHvURY8ZUhmjJKjnt5ml2w2-mCZxzVYSBYznAZNw9knRnEGbSfUhRACN86ItbfPnrK0jU7BbDu7il9nhZOt_4r6OJQu5BbPAK7mBcGBg_V0Fb7DcqfYj5G7qyrN5Bs9Z7EFRvWrUKp7UpItlGKtjgxK9TKeG3DszZRrQPL1NLQcgaxqU46PIfsvWw5jOL0i7LisWsY7q6nh7t40D2neR-ImqVsIvyGTUlY_KpyMDr5Di5OCc0ds0qO-Ha7vau3KO7bnp7ZVF6fhPqoXdCndVv5xz8NsHVC1FEbfEwH5pVIkiBZk7v4gACksunoqXffnaBsTA1bP7NARf2brKL7-1WxzAKseYyt5YTVvgdo1E0K1c9QRvmwzLlPWtRdp82uag3TTBC8eC4KvfNiVHuP3dvtnqisy__l8",
+            'authorization': bearerToken,
 
             // "device-id": deviceId
           }));
@@ -125,20 +125,18 @@ class NetworkApiServices extends BaseApiServices {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('token');
     try {
-      response = await dio.post(
-        url,
-        data: data,
-        options: token != null
-            ? Options(headers: {
-                "authorization": "Bearer $token",
-                //'access-token': token,
-              })
-            : Options(
-                headers: {
-                  "authorization": basicAuth,
-                },
-              ),
-      );
+      response = await dio.post(url,
+          data: data,
+          options: (token == null || token == "")
+              ? Options(
+                  headers: {
+                    "authorization": basicAuth,
+                  },
+                )
+              : Options(headers: {
+                  "authorization": "Bearer $token",
+                  //'access-token': token,
+                }));
     } on Exception catch (_) {
       return ResponseData<dynamic>(
           'Oops something Went Wrong', ResponseStatus.FAILED);
